688e2a1a9dbabb5c5ad954048cc99f223c9f528a
Jan Varmuza <jan.varmuza@gmail.com>
2021-09-25 17:25:43 +0200

    Output 2 dummy article with bs classes

--- server.js
+++ server.js
@@ -7,7 +7,19 @@ app.set("view engine", "ejs");
 app.use("/articles", articleRouter);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const articles = [
+    {
+      title: "Test title 1",
+      createdAt: new Date(),
+      description: "Test description 1"
+    },
+    {
+      title: "Test title 2",
+      createdAt: new Date(),
+      description: "Test description 2"
+    }
+  ];
+  res.render("index", { articles: articles });
 });
 
 app.listen(5000);
